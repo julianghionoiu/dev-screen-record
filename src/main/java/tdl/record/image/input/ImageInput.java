394253a53f9e0e885dfa01066ee7da838b09330e
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-05-13 14:55:27 +0100

    TIDY Fix javadoc issue [skip ci]

--- src/main/java/tdl/record/image/input/ImageInput.java
+++ src/main/java/tdl/record/image/input/ImageInput.java
@@ -14,6 +14,7 @@ public interface ImageInput {
      * Provide a sample of the input. This is required to allow the client to initialize the converters ahead of time
      *
      * @return A sample build with the format provided by the input
+     * @throws InputImageGenerationException it there was a problem creating the sample image
      */
     BufferedImage getSampleImage() throws InputImageGenerationException;
 
